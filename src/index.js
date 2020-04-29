0dc87bc00d27592af0f390a48fddd5957bbb81af
waldreznov@gmail.com <vladislav.ershov@vrpconsulting.com>
2020-04-29 21:33:46 +0300

    installed thunk, logger. Completed practic redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import {createStore} from './createStore';
-import {rootReducer} from './redux/rootReduser'
+import {createStore, applyMiddleware} from 'redux';
+import {rootReducer} from './redux/rootReduser';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import {increment, decrement, asyncIncrement, changeTheme} from './redux/actions';
+
 import './styles.css';
 
 const counter = document.getElementById('counter');
@@ -8,30 +12,46 @@ const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
 const themeBtn = document.getElementById('theme');
 
-const store = createStore(rootReducer, 0);
-console.log(createStore(rootReducer, 0))
-window.store = store; 
+// function logger(state) {
+//   return function(next) {
+//     return function(action) {
+//       console.log('Prev State', state.getState());
+//       console.log('action', action);
+//       const newState = next(action);
+//       console.log('New state', newState)
+//       return newState;
+//     }
+//   }
+// }
+
+const store = createStore(
+  rootReducer, 
+  applyMiddleware(thunk, logger)
+);
 
 addBtn.addEventListener('click', () => {
-  store.dispatch({type: 'INCREMENT' });
+  store.dispatch(increment());
 })
 
 subBtn.addEventListener('click', () => {
-  store.dispatch({type: 'DECREMENT' });
+  store.dispatch(decrement());
 })
 
 asyncBtn.addEventListener('click', () => {
-  store.dispatch({type: 'INCREMENT' });
+  store.dispatch(asyncIncrement());
 })
 
 store.subscribe(() => {
   const state = store.getState();
 
-  counter.textContent = state;
+  counter.textContent = state.counter;
+  document.body.className = state.theme.value;
 })
 
 store.dispatch({type: 'INIT_APPLICATION'})
 
 themeBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
+  const newTheme = document.body.classList.contains('light')
+    ? 'dark' : 'light';
+  store.dispatch(changeTheme(newTheme));
 })
--- src/redux/rootReduser.js
+++ src/redux/rootReduser.js
@@ -1,9 +1,29 @@
-export function rootReducer(state, action) {
-  if(action.type === 'INCREMENT') {
+import {INCREMENT, DECREMENT, ASYNC_INCREMENT, CHANGE_THEME} from './types';
+import { combineReducers } from 'redux';
+
+export function counterReducer(state = 0, action) {
+  if(action.type === INCREMENT) {
     return state + 1;
-  } else if (action.type === 'DECREMENT') {
+  } else if (action.type === DECREMENT) {
     return state - 1;
-  }
+  } 
 
   return state 
 }
+
+const initialThemeState = {
+  value: 'light'
+}
+
+export function themeReducer(state = initialThemeState, action) {
+  switch (action.type) {
+    case CHANGE_THEME: 
+      return {...state, value: action.payload}
+    default: return state
+  }
+}
+
+export const rootReducer = combineReducers({
+  counter: counterReducer,
+  theme: themeReducer
+})
